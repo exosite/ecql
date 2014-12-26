3b9e827bec9c33b278e7e7af21c19895177de5a9
Dominic Letz <dominicletz@exosite.com>
2014-12-26 12:04:48 +0800

    Decreased batch size limit even further to 40.

--- src/ecql_stream.erl
+++ src/ecql_stream.erl
@@ -35,7 +35,7 @@
 %% Defines
 -define(TIMEOUT, infinity).
 -define(MAX_PENDING, 100).
--define(BATCH_SIZE, 50).
+-define(BATCH_SIZE, 40).
 -define(MAX_PENDING_BATCH, 5).
 -define(DISABLED_PAGING, #paging{flag = 0}).
 
